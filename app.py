809cba16d933b298fc159aabf3bc78da523d8b74
Adam Gunn <asgunn@umich.edu>
2022-12-24 02:36:30 -0600

    serving app with waitress instead of flask default

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import uuid
 import hashlib
 from dotenv import load_dotenv
 import time
+from waitress import serve
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -197,4 +199,4 @@ def handle_delete():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080, extra_files=["static/style.css"])
+    serve(app, host="0.0.0.0", port=8080)
